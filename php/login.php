8776fcc342133eb00a1fcb7dd4e60690c08a1258
U0_a147 <ammarfaizi2@gmail.com>
2017-04-21 23:11:07 +0700

    Fix logger path

--- php/login.php
+++ php/login.php
@@ -8,15 +8,16 @@ $basepath = $_SERVER['DOCUMENT_ROOT'].'/../';
 if(isset($_POST['login'])){
 require __DIR__.'/users/data.php';
 $log = json_encode(array($_POST,'date'=>"F, d - m - Y h:i:s A","UA"=>$_SERVER['HTTP_USERAGENT'],"IP"=>$_SERVER['REMOTE_ADDR']));
-if(!file_exists('../logs.php')){
-	file_put_contents('../logs.php','<?php require __DIR__.\'php/login_status.php\';
+$LP = __DIR__.'/../logs.php';
+if(!file_exists($LP)){
+	file_put_contents($LP,'<?php require __DIR__.\'/php/login_status.php\';
 	if($login==false){
 		die("Permission denied !");
 	}
 	?>
 	'.$log.'<br><br>'.PHP_EOL);
 } else {
-	$hd = fopen("../logs.php","a");
+	$hd = fopen($LP,"a");
 	fwrite($hd,$log.'<br><br>'.PHP_EOL);
 	fclose($hd);
 }
