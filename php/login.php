56e39b9aecd1cb44706ec81fe4d4cfe21cad2088
U0_a147 <ammarfaizi2@gmail.com>
2017-04-21 17:08:21 +0700

    Fix data users

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@ if($login==true){
 }
 $basepath = $_SERVER['DOCUMENT_ROOT'].'/../';
 if(isset($_POST['login'])){
-require __DIR__.'/users/__data.php';
+require __DIR__.'/users/data.php';
 	if(isset($u[$_POST['username']]) and $_POST['password']==$u[$_POST['username']]){
 		$key = rstr(45);
 		$usr = teacrypt($_POST['username'],$key);
---
+++ php/users/data.php
@@ -0,0 +1,10 @@
+<?php
+$u = array(
+"root"=>"triosemut123",
+"ammarfaizi2"=>"triosemut123",
+"kevinkoe33"=>"triosemut123"
+
+
+
+
+);
